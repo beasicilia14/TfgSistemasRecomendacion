f893128ca1eae9a0b1cd7e7ebfca6a6be0b083d9
beasicilia14 <beatrizsiciliag14@gmail.com>
2023-03-02 17:53:06 -0500

    updating popularity alg

--- algorithms/popularity.py
+++ algorithms/popularity.py
@@ -3,34 +3,15 @@
 #CALCULAR SCORE DE TODOS LOS PUNTOS DE INTERÉS EN TRAIN. 
 #Nº Veces que un POI ha sido visitado por gente distinta 
 
-def popularityAlgorithm(validationset,trainset,user):
-    # Dictionary of POIs with visit counts
-
-    pois ={}
-
-    #Not using validation for this one as we don't have any parameters. 
-    with open(validationset, 'r') as file1:
-        file1_content = file1.read()
-
-    with open(trainset, 'a') as file2:
-        file2.write(file1_content)
-
-    # Calculating score (visit counts)
-    with open(trainset) as train:
-        for line in train:
-            split_line =line.split("\t")
-            user_id=split_line[0]
-            venue_id =split_line[1]
-
-            #check para no recomendar POI que el user ya haya ido
-            if user_id !=user:
-                if venue_id in pois.keys(): 
-                    pois[venue_id] += 1
-                else:
-                    pois[venue_id] =1
-
-    
-    pois_sorted = dict(sorted(pois.items(),key=lambda x: x[1], reverse=True))
+def popularityAlgorithm(scores,pois, user, numRecom): 
+    validos={}
+    #scores es diccionario poi_id:score
+    user_visited=pois[user]
+    for i,v in scores.items(): 
+        if i not in user_visited: 
+            validos[i] = v
+      
+    pois_sorted = dict(sorted(validos.items(),key=lambda x: x[1], reverse=True))
 
      # Return top 30 POIs with visit counts
     pois_top30 = dict(list(pois_sorted.items())[:30])
@@ -38,5 +19,3 @@ def popularityAlgorithm(validationset,trainset,user):
     with open("algorithms//PopularityRecommendations_user" + user + ".txt", "w") as file:
         for i, (poi, visits) in enumerate(pois_top30.items()):
             file.write(f"{i}\t{poi}\t{visits}\n")
-
-    return file
