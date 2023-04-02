26ce577513cf49a99f1c5fdeb991163cff764573
beasicilia14 <beatrizsiciliag14@gmail.com>
2023-04-02 19:28:20 -0400

    adding knn with midpoint scoring

--- clases/prueba_recomendadores.py
+++ clases/prueba_recomendadores.py
@@ -5,10 +5,12 @@ objeto_rand = Random()
 objeto_pop = Popularity()
 objeto_knn = Knn()
 
+objeto_knnmidpoint = KnnMidpoint()
 
 pois, scores, city = objeto_rec.readTrain("subsets/NewYork_US_train.txt")
 
 userSquaredSum, user_poi_scores, city= objeto_knn.readknn("subsets/NewYork_US_train.txt")
+
 
 
 with open("subsets/NewYork_US_validation.txt") as filevalidation: 
@@ -20,10 +22,15 @@ with open("subsets/NewYork_US_validation.txt") as filevalidation:
         #2: timestamp 
 
         #random
-        objeto_rand.recomendar(pois,user, 20, city, "clases\\Recomendaciones")
+        #objeto_rand.recomendar(pois,user, 20, city, "clases\\Recomendaciones")
         #popularity
-        objeto_pop.recomendar(pois,user, 20, city, scores,"clases\\Recomendaciones")
+        #objeto_pop.recomendar(pois,user, 20, city, scores,"clases\\Recomendaciones")
         #knn
-        objeto_knn.recomendar(userSquaredSum, user_poi_scores, user, 20, city, "clases\\Recomendaciones")
+        #objeto_knn.recomendar(userSquaredSum, user_poi_scores, user, 20, city, "clases\\Recomendaciones")
+
+        #knn midpoint 
+        objeto_knnmidpoint.recomendar("subsets//NewYork_US_train.txt", user, 20, "clases\\Recomendaciones")
+
+
 
 
